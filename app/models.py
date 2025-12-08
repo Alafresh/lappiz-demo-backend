481f467230da0ecbebda9674b92dc231baff19d2
juan_ <alafresh16@gmail.com>
2025-12-08 16:32:59 -0500

    feat(app): correcion typo EmailStr

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, EmailStrn, Field
+from pydantic import BaseModel, EmailStr, Field
 
 class Person(BaseModel):
     nombre: str = Field(..., min_length=4, max_length=60)
-    correo: EmailStrn
+    correo: EmailStr
