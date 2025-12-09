529af6af1fcbb8ba37c5ad4736b27b154d0bd075
juan_ <alafresh16@gmail.com>
2025-12-08 23:49:55 -0500

    (refactor) base model and endpoints
    se agrego una clase DTO y se agrego los atributos de id y fecha

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
-from models import Person
+from models import Person, PersonCreate
 from store import get_people, add_person
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Lappiz Demo Api", version="1.0.0")
 
-@app.get("/getPeople", response_model=list[Person], status_code=200)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/getPeople", response_model=list[Person], status_code=200, operation_id="getPeople")
 def getPeople():
     return get_people()
 
-@app.post("/addPerson", response_model=Person, status_code=201)
-def addPerson(person: Person):
-    return add_person(person)
+@app.post("/addPerson", response_model=Person, status_code=201, operation_id="addPerson")
+def addPerson(person_data: PersonCreate):
+    new_person = Person(
+        nombre=person_data.nombre,
+        correo=person_data.correo
+    )
+    return add_person(new_person)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel, EmailStr, Field
+from uuid import uuid4
+from datetime import datetime
 
-class Person(BaseModel):
+class PersonCreate(BaseModel):
     nombre: str = Field(..., min_length=4, max_length=60)
     correo: EmailStr
+
+class Person(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid4()))
+    nombre: str = Field(..., min_length=4, max_length=60)
+    correo: EmailStr
+    fecha: datetime = Field(default_factory=datetime.now)
